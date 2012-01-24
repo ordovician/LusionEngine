35710f4768228d072085c8754eb485c830d6f68e
Erik Engheim <erik.engheim@mac.com>
2012-01-24 22:56:07 +0100

    Preparation to make the project open source.
    All files are intended to be GPLv2.0 although I haven't updated all headers.

--- Geometry/Trapezoid2.hpp
+++ Geometry/Trapezoid2.hpp
@@ -1,12 +1,21 @@
-/******************************************************************
-Name	: Trapezoid2
-Author	: Erik Engheim
-Date	: 07/09/2005
-Desc	: Describes a 2d line segment. 
-Comment	: This class has not been tested yet.
+/*
+	LusionEngine- 2D game engine written in C++ with Lua interface.
+	Copyright (C) 2006  Erik Engheim
 
-Copyright 2007 Translusion. All rights reserved.
-*******************************************************************/
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 
 #pragma once
 
--- Geometry/TrapezoidNode2.hpp
+++ Geometry/TrapezoidNode2.hpp
@@ -1,12 +1,21 @@
-/******************************************************************
-Name	: TrapezoidNode2
-Author	: Erik Engheim
-Date	: 06/02/2006
-Desc	: A node in a tree used a a search tree for trapezoids in a trapezoidal map.
-Comment	: This class has not been tested yet.
+/*
+	LusionEngine- 2D game engine written in C++ with Lua interface.
+	Copyright (C) 2006  Erik Engheim
 
-Copyright 2007 Translusion. All rights reserved.
-*******************************************************************/
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 
 #pragma once
 
@@ -21,6 +30,9 @@ Copyright 2007 Translusion. All rights reserved.
 #include <vector>
 #include <string>
 
+/**
+  A node in a tree used a a search tree for trapezoids in a trapezoidal map.
+*/
 class TrapezoidNode2
 {
 public:
